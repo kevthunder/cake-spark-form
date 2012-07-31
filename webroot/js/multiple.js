2beafd38658a05d178d107c3ddb5605e2ad851c2
kgiguere <kgiguere@9d2c35e4-253f-4dd7-b3e9-596db99104a9>
2012-07-31 13:32:06 +0000

    git-svn-id: svn+ssh://208.111.94.3/svn/plugins/o2form/trunk/o2form@182 9d2c35e4-253f-4dd7-b3e9-596db99104a9

--- webroot/js/multiple.js
+++ webroot/js/multiple.js
@@ -1,48 +1,4 @@
-(function( $ ) {
-	$(function(){
-		$('.MultipleTable .btDelete').live('click',function (){
-			$line = $(this).closest('.line');
-			multipleDeleteRow($line);
-			return false;
-		});
-		$('.MultipleTable .btAdd').live('click',function (){
-			$table = $(this).closest('.MultipleTable');
-			activeCount = $('.line:not(.modelLine, .deletedLine)',$table).length;
-			if($table.attr('max') && activeCount+1 >= $table.attr('max')){
-				$(this).addClass('.disabled');
-				if(activeCount >= $table.attr('max')){
-					return false
-				}
-			}
-			$model = $('.modelLine',$table);
-			$last = $('.line:last',$table);
-			$clone = $model.clone();
-			$clone.removeClass('modelLine').find('input, textarea, select').attr('disabled',false);
-			count = $('.line:not(.modelLine)',$table).length;
-			$clone.html($clone.html().replace(/---i---/g,count));
-			$last.after($clone);
-			return false;
-		});
-		
-		$('.MultipleTable input, .MultipleTable textarea, .MultipleTable select').filter('[spc=deleteInput]').each(function (){
-			if($(this).val() == 1){
-				$line = $(this).closest('.line');
-				multipleDeleteRow($line);
-			}
-		});
-	});
-	window.multipleDeleteRow = function($line){
-		$table = $($line).closest('.MultipleTable');
-		activeCount = $('.line:not(.modelLine, .deletedLine)',$table).length;
-		if($table.attr('max') && activeCount-1 < $table.attr('max')){
-			$('.btAdd',$table).removeClass('.disabled');
-		}
-		$line.addClass('deletedLine').find('input, textarea, select').filter(':not([spc=keyInput],[spc=deleteInput])').attr('disabled',true);
-		$('input, textarea, select',$line).filter('[spc=deleteInput]').val(1);
-	};
-})( jQuery );
 
-/*
 (function( $ ){
 
   var methods = {
@@ -55,13 +11,25 @@
          
          // If the plugin hasn't been initialized yet
          if ( ! data ) {
-         
-           
+			
+			$('.btAdd',this).live('click.multiple',function (){
+				$(this).multiple('addRow');
+				return false;
+			});
+			$('.btDelete',this).live('click.multiple',function (){
+				$(this).multiple('deleteRow');
+				return false;
+			});
+			
+			$('input, textarea, select',this).filter('[spc=deleteInput]').each(function (){
+				if($(this).val() == 1){
+					$line = $(this).closest('.line');
+					$line.multiple('deleteRow');
+				}
+			});
 
-           $(this).data('multiple', {
-               target : $this,
-               multiple : multiple
-           });
+			$(this).data('multiple', {
+			});
 
          }
        });
@@ -80,19 +48,89 @@
        })
 
      },
+	 getContainer:function(){
+		if($(this).is(".MultipleTable")){
+			return $(this);
+		}else{
+			return $(this).closest('.MultipleTable');
+		}
+	 },
 	 deleteRow : function(){
-        return this.each(function(){
+        return this.each(function(key){
 			var $line;
 			if($(this).is(".line")){
 				$line = $(this);
 			}
-			if($(this).closest('.line').length && $(this).closest('.MultipleTable').length){
-				
+			if(!$line && $(this).closest('.line').length && $(this).closest('.MultipleTable').length){
+				$line = $(this).closest('.line');
+			}
+			if(!$line){
+				$table = $(this).multiple('getContainer');
+			}else{
+				$table = $line.multiple('getContainer');
+			}
+			if($table && $table.length){
+				var activeCount = $('.line:not(.modelLine, .deletedLine)',$table).length;
+				if(!$line && arguments.length == 1){
+					if($('.line[spc=keyInput][val='+key+']').length){
+						$line = $('.line[spc=keyInput][val='+key+']');
+					}
+					if(!$line && !isNaN($key) && key < activeCount){
+						$line = $('.line:not(.modelLine, .deletedLine)',$table)[key];
+					}
+				}
+				if($line){
+					if($table.attr('max') && activeCount-1 < $table.attr('max')){
+						$('.btAdd',$table).removeClass('.disabled');
+					}
+					$line.addClass('deletedLine').find('input, textarea, select').filter(':not([spc=keyInput],[spc=deleteInput])').attr('disabled',true);
+					$('input, textarea, select',$line).filter('[spc=deleteInput]').val(1);
+				}
+			}
+		});
+	 },
+	 nbRows : function(nb){
+		var $table = this.multiple('getContainer');
+		$table = $table.eq(0);
+		var activeCount = $('.line:not(.modelLine, .deletedLine)',$table).length;
+		if(arguments.length == 1 && !isNaN(nb)){
+			if(activeCount < nb){
+				$table.multiple('addRow',nb-activeCount);
+			}else if(activeCount > nb){
+				$('.line:not(.modelLine, .deletedLine)',$table).slice(nb).multiple('deleteRow');
 			}
 		}
+		return activeCount;
+	 },
+	 addRow : function(nb){
+		var $table = this.multiple('getContainer');
+		if($table && $table.length){
+			return $table.each(function(key){
+				if(arguments.length == 0 || isNaN(nb)){
+					nb = 1;
+				}
+				var $table = $(this);
+				for(var i=0;i<nb;i++){
+					var activeCount = $('.line:not(.modelLine, .deletedLine)',$table).length;
+					if($table.attr('max') && activeCount+1 >= $table.attr('max')){
+						$('.btAdd',$table).addClass('.disabled');
+						if(activeCount >= $table.attr('max')){
+							return false
+						}
+					}
+					var $model = $('.modelLine',$table);
+					var $last = $('.line:last',$table);
+					var $clone = $model.clone();
+					$clone.removeClass('modelLine').find('input, textarea, select').attr('disabled',false);
+					var count = $('.line:not(.modelLine)',$table).length;
+					$clone.html($clone.html().replace(/---i---/g,count));
+					$last.after($clone);
+				}
+			});
+		}
+		return false;
 	 }
   };
-
   $.fn.multiple = function( method ) {
     
     if ( methods[method] ) {
@@ -105,4 +143,9 @@
   
   };
 
-})( jQuery );*/
+  
+	$(function(){
+		$('.MultipleTable').multiple();
+	});
+  
+})( jQuery );
